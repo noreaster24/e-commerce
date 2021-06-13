29a41493fb67d7d8943b6c7ad7a3e286609337d6
Joe Porrazzo <joepa2624@me.com>
2021-06-12 21:11:30 -0700

    set index Model associations

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Products.belongsTo(Category, {
+  foreignKey: 'id'
+});
 
 // Categories have many Products
+Categories.hasMany(Products, {
+  foreignKey: 'id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Products.belongToMany(Tags, {
+  through: ProductTag,
+  as: '',
+  foreignKey: 'id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Products, {
+  through: ProductTag,
+  as: '',
+  foreignKey: 'id'
+});
 
 module.exports = {
   Product,
